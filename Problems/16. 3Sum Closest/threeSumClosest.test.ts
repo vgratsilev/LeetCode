acd37751305b2f446eb99d0fc58f65b6f07047f7
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2023-05-09 19:05:28 +0500

    18. 4Sum

--- Problems/16. 3Sum Closest/threeSumClosest.test.ts
+++ Problems/16. 3Sum Closest/threeSumClosest.test.ts
@@ -1,11 +1,11 @@
 import { threeSumClosest } from './threeSumClosest';
 
 describe('16. 3Sum Closest', () => {
-    test('Example 1', async() => {
+    test('Example 1', () => {
         expect(threeSumClosest([ -1, 2, 1, -4 ], 1)).toBe(2);
     });
 
-    test('Example 2', async() => {
+    test('Example 2', () => {
         expect(threeSumClosest([ 0, 0, 0 ], 1)).toBe(0);
     });
 });
---
+++ Problems/18. 4Sum/fourSum.test.ts
@@ -0,0 +1,11 @@
+import { fourSum } from './fourSum';
+
+describe('16. 3Sum Closest', () => {
+    test('Example 1', () => {
+        expect(fourSum([ 1, 0, -1, 0, -2, 2 ], 0)).toEqual([ [ -2, -1, 1, 2 ], [ -2, 0, 0, 2 ], [ -1, 0, 0, 1 ] ]);
+    });
+
+    test('Example 2', () => {
+        expect(fourSum([ 2, 2, 2, 2, 2 ], 8)).toEqual([ [ 2, 2, 2, 2 ] ]);
+    });
+});
