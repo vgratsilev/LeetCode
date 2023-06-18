351b2c04987444a39d53ef462d9fa7a11bf1d27b
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2023-06-18 19:45:48 +0500

    507. Perfect Number

--- Problems/507. Perfect Number/checkPerfectNumber.ts
+++ Problems/507. Perfect Number/checkPerfectNumber.ts
@@ -1,16 +1,16 @@
 export function checkPerfectNumber(num: number): boolean {
-    if (num === 1) {
+    if(num === 1) {
         return false;
     }
 
     let output = 0;
 
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (!(num % i)) {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(!(num % i)) {
             const tmp = num / i !== i ? (num / i + i) : i;
             output += tmp;
         }
     }
 
     return output + 1 === num;
-};
+}
